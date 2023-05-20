f43bd508a0baad81e523e326da5d75d85b281194
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2023-05-20 14:53:15 +0200

    save

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+import linkify from '@/global_scripts/linkify.js'
+import drag from '@/global_scripts/drag.js'
+
+let var_zoom = 1
+
+function zoom(e){
+  var_zoom += -e.deltaY/10000
+  document.querySelector("body > #app > main").style.transform=`scale(${var_zoom}) translate(${e.pageX*(var_zoom-1)-window.innerWidth*(var_zoom-1)/10}px, ${e.pageY*(var_zoom-1)-window.innerHeight*(var_zoom-1)/10}px)`
+}
+
+document.body.addEventListener("wheel",zoom)
+
+</script>
+
+<template>
+  <main style="position: relative;">
+    <TheWelcome/>
+  </main>
+</template>
+
+<style>
+@import url("@/assets/base.css");
+</style>
---
+++ src/components/Wire.vue
@@ -0,0 +1,10 @@
+<script>
+
+
+</script>
+
+<template>
+
+    <div style="display: block;background-color: black;border-radius: 9em;"></div>
+
+</template>
